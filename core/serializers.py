3d9f9dc8110fce2c5f74c93df8d44370c4c398b2
Serafeim Papastefanos <spapas@gmail.com>
2018-02-14 13:11:54 +0200

    Some small changes

--- core/serializers.py
+++ core/serializers.py
@@ -1,32 +1,39 @@
 from rest_framework import serializers
 import core.models
 
+
+class GenreSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    class Meta:
+        model = core.models.Genre
+        fields = '__all__'
+
+
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    genres = GenreSerializer(many=True, )
     class Meta:
         model = core.models.Movie
         fields = '__all__'
         # fields = ('url', 'username', 'email', 'is_staff')
 
 
-class GenreSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = core.models.Genre
-        fields = '__all__'
-
-
 class JobSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
     class Meta:
         model = core.models.Job
         fields = '__all__'
 
 
 class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
     class Meta:
         model = core.models.Person
         fields = '__all__'
 
 
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
     class Meta:
         model = core.models.Image
         fields = '__all__'
--- core/views.py
+++ core/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from rest_framework import routers, serializers, viewsets
+from rest_framework.pagination import PageNumberPagination
 import core.models
 import core.serializers
+
+
+class SmallPaginator(PageNumberPagination):
+    page_size = 10
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = core.models.Movie.objects.all()
     serializer_class = core.serializers.MovieSerializer
+    pagination_class = SmallPaginator
 
 
 class JobViewSet(viewsets.ModelViewSet):
@@ -23,6 +29,7 @@ class GenreViewSet(viewsets.ModelViewSet):
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = core.models.Person.objects.all()
     serializer_class = core.serializers.PersonSerializer
+    pagination_class = SmallPaginator
 
 
 class ImageViewSet(viewsets.ModelViewSet):
