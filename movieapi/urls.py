9119d8a067d4385b52cd82ba4bf36fce721e8f98
Serafeim Papastefanos <spapas@gmail.com>
2018-02-15 13:01:14 +0200

    Add a catch all view

--- movieapi/urls.py
+++ movieapi/urls.py
@@ -16,9 +16,10 @@ router.register(r'genres', core.views.GenreViewSet)
 router.register(r'images', core.views.ImageViewSet)
 
 urlpatterns = [
-    url(r'^$', core.views.HomeTemplateView.as_view(), name='home', ),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     path('admin/', admin.site.urls),
+
+    url(r'^.*', core.views.HomeTemplateView.as_view(), name='home', ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
