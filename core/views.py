c7ef752401df2582c6953fa8baf492ed6d4f6a64
Serafeim Papastefanos <spapas@gmail.com>
2018-03-23 17:18:21 +0200

    Add filtering to the API

--- core/views.py
+++ core/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from rest_framework import routers, serializers, viewsets
 from rest_framework.pagination import PageNumberPagination
+from django_filters import rest_framework as filters
 import core.models
+import core.filters
 import core.serializers
 
 
@@ -14,22 +16,30 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = core.models.Movie.objects.all().order_by('id')
     serializer_class = core.serializers.MovieSerializer
     pagination_class = SmallPaginator
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = core.filters.MovieFilter
 
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = core.models.Job.objects.all().order_by('id')
     serializer_class = core.serializers.JobSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = core.filters.JobFilter
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = core.models.Genre.objects.all().order_by('id')
     serializer_class = core.serializers.GenreSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = core.filters.GenreFilter
 
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = core.models.Person.objects.all().order_by('id')
     serializer_class = core.serializers.PersonSerializer
     pagination_class = SmallPaginator
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = core.filters.PersonFilter
 
 
 class ImageViewSet(viewsets.ModelViewSet):
